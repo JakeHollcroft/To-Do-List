d005d4a7ed111210060c5bfa4508a764bb088c9d
Jake Hollcroft <41431741+JakeHollcroft@users.noreply.github.com>
2018-07-21 09:59:02 -0600

    To-Do List
    I created this To-Do List with CSS, HTML, and JavaScript.  You can add new items, delete items, and mark items as done.
    
    http://nm-app.com/todo/index.html

---
+++ assets/js/todos.js
@@ -0,0 +1,31 @@
+var name = prompt("What is your name?");
+
+$("h1").append(name + "'s To-Do List:");
+
+// Check Off Specific Item By Clicking
+$("ul").on("click", "li", function(){
+	$(this).toggleClass("completed");
+});
+
+// Click on X to delete Item
+$("ul").on("click", "span", function(event){
+	$(this).parent().fadeOut(500, function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event){
+	if(event.which === 13){
+		//grabbing new item text from input
+		var todoText = $(this).val();
+		$(this).val("");
+		//create new li and add to ul
+		$("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todoText +  "</li>");
+	}
+});
+
+$(".fa-plus").click(function(){
+	$("input[type='text']").fadeToggle();
+})
+
